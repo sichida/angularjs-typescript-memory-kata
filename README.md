fa92c2a3c7cbb7e3103add953a2e3b86310bb26b
Shoun Ichida <shoun@ichida.fr>
2016-05-25 14:10:53 +0200

    Project initialization

---
+++ README.md
@@ -0,0 +1,4 @@
+# AngularJS kata using TypeScript
+
+This kata aims to create a [_Memory_ game](https://en.wikipedia.org/wiki/Concentration_%28game%29).
+
---
+++ jspm.config.js
@@ -0,0 +1,37 @@
+SystemJS.config({
+  transpiler: "plugin-typescript",
+  packages: {
+    "app": {
+      "main": "app.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-typescript"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "github:*/*.json",
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {
+    "os": "github:jspm/nodelibs-os@0.2.0-alpha",
+    "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.16"
+  },
+  packages: {
+    "github:frankwallis/plugin-typescript@4.0.16": {
+      "map": {
+        "typescript": "npm:typescript@1.8.10"
+      }
+    },
+    "github:jspm/nodelibs-os@0.2.0-alpha": {
+      "map": {
+        "os-browserify": "npm:os-browserify@0.2.1"
+      }
+    }
+  }
+});
