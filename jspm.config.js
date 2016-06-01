6c9d2fa4701a1d426ec13406d75c8705c5867156
Shoun Ichida <shoun@ichida.fr>
2016-06-01 13:55:29 +0200

    Replacing angular-router by ui-router

--- jspm.config.js
+++ jspm.config.js
@@ -22,18 +22,13 @@ SystemJS.config({
   map: {
     "angular": "github:angular/bower-angular@1.5.6",
     "angular-resource": "github:angular/bower-angular-resource@1.5.6",
-    "angular-route": "github:angular/bower-angular-route@1.5.6",
+    "angular-ui-router": "github:angular-ui/angular-ui-router-bower@0.3.0",
     "os": "github:jspm/nodelibs-os@0.2.0-alpha",
     "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.16",
     "text": "github:systemjs/plugin-text@0.0.8"
   },
   packages: {
     "github:angular/bower-angular-resource@1.5.6": {
-      "map": {
-        "angular": "github:angular/bower-angular@1.5.6"
-      }
-    },
-    "github:angular/bower-angular-route@1.5.6": {
       "map": {
         "angular": "github:angular/bower-angular@1.5.6"
       }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import angular from 'angular';
-import 'angular-route';
+import 'angular-ui-router';
 import 'angular-resource';
 import memoryView from './features/memory/memory.view.html!text';
 
 import {MemoryController} from './features/memory/memory.controller.ts';
 
-angular.module('typescript-kata', ['ngRoute'])
-    .config($routeProvider => {
-        $routeProvider.when('/', {
+angular.module('typescript-kata', ['ui.router'])
+    .config($stateProvider => {
+        $stateProvider.state('home', {
+            url: '/',
             template: memoryView,
             controller: MemoryController,
             controllerAs: 'ctrl'
