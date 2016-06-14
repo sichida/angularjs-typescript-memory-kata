fc22043eb28d0c5bd931c20ad09881ad0e672c6e
Shoun Ichida <shoun@ichida.fr>
2016-06-14 12:12:39 +0200

    Removing dead code

--- src/features/memory/memory.controller.ts
+++ src/features/memory/memory.controller.ts
@@ -6,6 +6,5 @@ export class MemoryController {
     
     constructor() {
         this.card = new Card('https://pixabay.com/static/uploads/photo/2014/04/20/12/30/thumb-328420_640.jpg');
-        // this.card.imageUrl = 'https://pixabay.com/static/uploads/photo/2014/04/20/12/30/thumb-328420_640.jpg';
     }
 }
