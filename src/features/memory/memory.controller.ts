30922441882d5660793dc2d8327c2edea1a38436
Shoun Ichida <shoun@ichida.fr>
2016-06-14 14:32:01 +0200

    Adding cards with images to be used in example

--- src/features/memory/memory.controller.ts
+++ src/features/memory/memory.controller.ts
@@ -1,10 +1,13 @@
+import './memory.css';
 import {Card} from '../card/card.component.ts';
+import {card1, cards4x4} from '../../services/card.service.ts';
 
 export class MemoryController {
     hello: string = 'Hello World!';
     card: Card;
     
     constructor() {
-        this.card = new Card('https://pixabay.com/static/uploads/photo/2014/04/20/12/30/thumb-328420_640.jpg');
+        this.card = card1;
+        this.cards = cards4x4;
     }
 }
---
+++ src/services/card.service.ts
@@ -0,0 +1,53 @@
+import {Card} from '../features/card/card.component.ts';
+
+export const card1: Card = new Card('/src/assets/cards-260/1.png');
+export const card2: Card = new Card('/src/assets/cards-260/2.png');
+export const card3: Card = new Card('/src/assets/cards-260/3.png');
+export const card4: Card = new Card('/src/assets/cards-260/4.png');
+export const card5: Card = new Card('/src/assets/cards-260/5.png');
+export const card6: Card = new Card('/src/assets/cards-260/6.png');
+export const card7: Card = new Card('/src/assets/cards-260/7.png');
+export const card8: Card = new Card('/src/assets/cards-260/8.png');
+export const card9: Card = new Card('/src/assets/cards-260/9.png');
+export const card10: Card = new Card('/src/assets/cards-260/10.png');
+export const card11: Card = new Card('/src/assets/cards-260/11.png');
+export const card12: Card = new Card('/src/assets/cards-260/12.png');
+export const card13: Card = new Card('/src/assets/cards-260/13.png');
+export const card14: Card = new Card('/src/assets/cards-260/14.png');
+export const card15: Card = new Card('/src/assets/cards-260/15.png');
+export const card16: Card = new Card('/src/assets/cards-260/16.png');
+export const card17: Card = new Card('/src/assets/cards-260/17.png');
+export const card18: Card = new Card('/src/assets/cards-260/18.png');
+export const card19: Card = new Card('/src/assets/cards-260/19.png');
+export const card20: Card = new Card('/src/assets/cards-260/20.png');
+export const card21: Card = new Card('/src/assets/cards-260/21.png');
+export const card22: Card = new Card('/src/assets/cards-260/22.png');
+export const card23: Card = new Card('/src/assets/cards-260/23.png');
+export const card24: Card = new Card('/src/assets/cards-260/24.png');
+export const card25: Card = new Card('/src/assets/cards-260/25.png');
+export const card26: Card = new Card('/src/assets/cards-260/26.png');
+export const card27: Card = new Card('/src/assets/cards-260/27.png');
+export const card28: Card = new Card('/src/assets/cards-260/28.png');
+export const card29: Card = new Card('/src/assets/cards-260/29.png');
+export const card30: Card = new Card('/src/assets/cards-260/30.png');
+export const card31: Card = new Card('/src/assets/cards-260/31.png');
+export const card32: Card = new Card('/src/assets/cards-260/32.png');
+export const card33: Card = new Card('/src/assets/cards-260/33.png');
+export const card34: Card = new Card('/src/assets/cards-260/34.png');
+export const card35: Card = new Card('/src/assets/cards-260/35.png');
+export const card36: Card = new Card('/src/assets/cards-260/36.png');
+export const card37: Card = new Card('/src/assets/cards-260/37.png');
+
+export const cards4x4: Array<Card> = [
+    card1, card2, card3, card4
+];
+export const cards8x8: Array<Card> = [
+    card1, card2, card3, card4,
+    card5, card6, card7, card8
+];
+export const allCards: Array<Card> = [
+    card1, card2, card3, card4, card5, card6, card7, card8, card9, card10,
+    card11, card12, card13, card14, card15, card16, card17, card18, card19, card20,
+    card21, card22, card23, card24, card25, card26, card27, card28, card29, card30,
+    card31, card32, card33, card34, card35, card36, card37
+]
